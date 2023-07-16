cee5191ff76dd63386d23daa545d7a14e9a500f5
Kevin <kevin@MacBook-Pro-de-Kevin.local>
2023-07-15 21:41:23 -0400

    first commit

--- App.js
+++ App.js
@@ -5,14 +5,36 @@ import { s } from "./App.style";
 import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
- 
+  const [imageURIList, setImageURIList] = useState([]);
+  async function pickImageAsync() {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (result.canceled) {
+      alert("Aucune image selectionnée");
+    } else {
+      setImageURIList([...imageURIList, result.assets[0].uri]);
+    }
+  }
+  console.log("***", imageURIList);
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
         <Text style={s.title}>Mes photos préférées</Text>
-        <View style={s.body}></View>
+        <View style={s.body}>
+          <ScrollView>
+            {imageURIList.map((imageURI, i) => (
+              <Image
+                style={{ height: 300, marginVertical: 30, width: "100%" }}
+                key={imageURI + i}
+                source={{ uri: imageURI }}
+              />
+            ))}
+          </ScrollView>
+        </View>
         <View style={s.footer}>
-          <TouchableOpacity style={s.btn} onPress={()=>""}>
+          <TouchableOpacity style={s.btn} onPress={pickImageAsync}>
             <Text style={s.btnTxt}>Ajouter photo</Text>
           </TouchableOpacity>
         </View>
